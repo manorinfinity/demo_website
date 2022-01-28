3c10aed834139e662042e7472eef04655f2f1ba0
Gyanendra <gyanendra2003@yahoo.co.in>
2022-01-28 21:02:33 +0530

    alo=most done landing pafe

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,38 @@ export default function Home() {
         <p className={Styles.tagline}>Your one stop financial soultion</p>
         <button className={Styles.comingSoon}>Coming Soon</button>
       </section>
-      <section className={Styles.ovalSection}></section>
+      <section className={Styles.ovalSection}>
+        <p>Suscribe for latest updates and early access</p>
+        <p className={Styles.loremStyle}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          <br /> eiusmod tempor incididunt ut
+        </p>
+        <div className={Styles.inputArea}>
+          <input
+            type="text"
+            name=""
+            id=""
+            className={Styles.inputBox}
+            placeholder="Enter email address"
+          />
+          <button className={Styles.searchButton}>Submit</button>
+        </div>
+      </section>
+      <section className={Styles.blogSection}>
+        <p className={Styles.checkBlog}>
+          Check Our latest blog posts and articles
+        </p>
+        <p className={Styles.tagLine}>
+          Join 40,000+ other marketers and get proven strategies on email
+          marketing
+        </p>
+        <div className={Styles.blogShowcase}>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+        </div>
+      </section>
       <Footer />
     </>
   );
---
+++ src/svgs/Oval.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const Oval = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={2800}
+    height={1053.333}
+    viewBox="0 0 2100 790"
+    {...props}
+  />
+);
+
+export default Oval;
