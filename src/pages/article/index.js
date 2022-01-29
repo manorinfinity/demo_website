b69fea69330b2820ec978cabd472fed65a627800
Gyanendra <gyanendra2003@yahoo.co.in>
2022-01-29 20:21:45 +0530

    almost done

--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -30,11 +30,76 @@ function Article() {
         </div>
       </div>
       <div className={Styles.readingSection}>
-        <div>
+        <div className={Styles.content}>
           <img
+            className={Styles.euroImage}
             src="https://images.unsplash.com/photo-1621280336935-ed7cae618aac?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxhbGx8fHx8fHx8fHwxNjIyNDA2NzYw&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
             alt="image not found"
           />
+          <div className={Styles.paras}>
+            <p>
+              Donec sed odio dui. Nullam id dolor id nibh ultricies vehicula ut
+              id elit. Donec id elit non mi porta gravida at eget metus.
+              Curabitur blandit tempus porttitor. Donec ullamcorper nulla non
+              metus auctor fringilla.
+            </p>
+            <p>
+              Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac
+              facilisis in, egestas eget quam. Vestibulum id ligula porta felis
+              euismod semper. Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit. Morbi leo risus, porta ac consectetur ac, vestibulum at
+              eros. Morbi leo risus, porta ac consectetur ac, vestibulum at
+              eros.
+            </p>
+            <p>
+              Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum
+              nibh, ut fermentum massa justo sit amet risus. Donec sed odio dui.
+              Integer posuere erat a ante venenatis dapibus posuere velit
+              aliquet. Vestibulum id ligula porta felis euismod semper. Donec
+              ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam.
+              Pellentesque ornare sem lacinia quam venenatis vestibulum. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <div className={Styles.paraWithPhoto}>
+            <img
+              className={Styles.euroImageSmall}
+              src="https://images.unsplash.com/photo-1579170053380-58064b2dee67?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+            />
+            <div className={Styles.smallPara}>
+              <p>
+                Donec sed odio dui. Nullam id dolor id nibh ultricies vehicula
+                ut id elit. Donec id elit non mi porta gravida at eget metus.
+                Curabitur blandit tempus porttitor. Donec ullamcorper nulla non
+                metus auctor fringilla.
+              </p>
+              <p>
+                Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac
+                facilisis in, egestas eget quam. Vestibulum id ligula porta
+                felis euismod semper. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Morbi leo risus, porta ac consectetur ac,
+                vestibulum at eros. Morbi leo risus, porta ac consectetur ac,
+                vestibulum at eros.
+              </p>
+              <p>
+                Fusce dapibus, tellus ac cursus commodo, tortor mauris
+                condimentum nibh, ut fermentum massa justo sit amet risus. Donec
+                sed odio dui. Integer posuere erat a ante venenatis dapibus
+                posuere velit aliquet. Vestibulum id ligula porta felis euismod
+                semper. Donec ullamcorper nulla non metus auctor fringilla.
+                Aenean eu leo quam. Pellentesque ornare sem lacinia quam
+                venenatis vestibulum. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit.
+              </p>
+            </div>
+          </div>
+          <div className={Styles.lastName}>
+            Donec sed odio dui. Nullam id dolor id nibh ultricies vehicula ut id
+            elit. Donec id elit non mi porta gravida at eget metus. Curabitur
+            blandit tempus porttitor. Donec ullamcorper nulla non metus auctor
+            fringilla. Curabitur blandit tempus porttitor. Donec ullamcorper
+            nulla non metus auctor fringilla.
+          </div>
         </div>
         <div>hi</div>
       </div>
